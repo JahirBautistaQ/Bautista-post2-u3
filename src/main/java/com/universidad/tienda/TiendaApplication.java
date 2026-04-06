aa844b6911f718965e290641f27b764d0e329117
JahirBautistaQ <02210132008@mail.udes.edu.co>
2026-04-05 20:33:20 -0500

    Paso 2 — Patrón Facade: Subsistema de Notificaciones

--- src/main/java/com/universidad/tienda/TiendaApplication.java
+++ src/main/java/com/universidad/tienda/TiendaApplication.java
@@ -1,13 +1,48 @@
 package com.universidad.tienda;
 
+import com.universidad.tienda.decorator.OrdenServicio;
+import com.universidad.tienda.facade.NotificacionFacade;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TiendaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TiendaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaApplication.class, args);
+    }
 
+    //PRUEBA DECORATOR
+    @Bean
+    public CommandLineRunner testDecorator(
+            @Qualifier("ordenCompleto") OrdenServicio servicio) {
+        return args -> {
+            System.out.println("----- TEST DECORATOR -----");
+            servicio.procesarOrden("ORD-001", 50000);
+        };
+    }
+
+    //PRUEBA FACADE
+    @Bean
+    public CommandLineRunner testFacade(NotificacionFacade facade) {
+        return args -> {
+            System.out.println("----- TEST FACADE -----");
+
+            facade.notificarCompraExitosa(
+                    "cliente@email.com",
+                    "3001234567",
+                    "TOKEN123",
+                    "ORD-001"
+            );
+
+            facade.notificarErrorPago(
+                    "cliente@email.com",
+                    "3001234567",
+                    "ORD-002"
+            );
+        };
+    }
 }
